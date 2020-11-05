f893350b57a755a6d643576c886fe6b1422073f8
Coopermakc <Coopermakc@mail.ru>
2020-11-05 21:12:55 +0300

    override create

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from app.models import Post, Category
 from app.serializers import PostSerializer, CategorySerializer
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import generics, status
 
 
 class PostList(generics.ListCreateAPIView):
@@ -16,6 +17,14 @@ class PostDetail(generics.RetrieveAPIView):
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def create(self, request):
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class CategoryDetail(generics.RetrieveAPIView):
     queryset = Category.objects.all().filter()
